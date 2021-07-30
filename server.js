8acecceaabf90de37bbe3937d9554f328741511a
Ryely <ryely.moore@outlook.com>
2021-07-30 17:10:02 -0600

    implemented session tracking through cookies

--- server.js
+++ server.js
@@ -4,22 +4,41 @@ const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// handlebars middlware
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+// session middleware
+app.use(session(sess));
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 // turn on routes
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({force: false }).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now Listening'));
 });
