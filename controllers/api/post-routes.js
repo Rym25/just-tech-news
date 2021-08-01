9620416445a71ed4238feba1b2352aef8157d5a8
Ryely <ryely.moore@outlook.com>
2021-07-31 18:05:35 -0600

    created functionality for upvoting and adding comments, also renders elements differently base on whether or not a user is logged in

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -46,9 +46,9 @@ router.get('/:id', (req, res) => {
             id: req.params.id
         },
         attributes: [
-            'id', 
-            'post_url', 
-            'title', 
+            'id',
+            'post_url',
+            'title',
             'created_at',
             [
                 sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
@@ -100,14 +100,17 @@ router.post('/', (req, res) => {
 
 // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
-    // custom static method created in models/Post.js
-    Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    });
-});
+    // make sure the session exists first
+    if (req.session) {
+      // pass session id along with all destructured properties on req.body
+      Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote })
+        .then(updatedVoteData => res.json(updatedVoteData))
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    }
+  });
 
 // Posts put route
 router.put('/:id', (req, res) => {
